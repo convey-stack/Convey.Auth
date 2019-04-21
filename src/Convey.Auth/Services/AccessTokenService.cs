5aaa9c5903d47da705bc8a20c03bbd59a65ee3bb
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-04-21 19:22:35 +0200

    Refactoring, code cleanup

--- src/Convey.Auth/Services/AccessTokenService.cs
+++ src/Convey.Auth/Services/AccessTokenService.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 
 namespace Convey.Auth.Services
